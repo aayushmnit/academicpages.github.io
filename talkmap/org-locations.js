5faf9574e2aa7b094c15348e31a0d6f1fb8adab6
agraw134 <agraw134@umn.edu>
2018-02-19 20:51:55 -0600

    Updating talk locations

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,22 +1,11 @@
 var addressPoints = [
   [
-    "Berkeley CA, USA",
-    37.8708393,
-    -122.2728638
+    "Minneapolis MN, USA",
+    44.9778,
+    -93.2650
   ],
   [
-    "London, UK",
-    51.5073219,
-    -0.1276473
-  ],
-  [
-    "San Francisco, California",
-    37.7792808,
-    -122.4192362
-  ],
-  [
-    "Los Angeles, CA",
-    34.0543942,
-    -118.2439408
-  ]
-];
+    "Jaipur, India",
+    26.9124,
+    75.7873
+  ];
